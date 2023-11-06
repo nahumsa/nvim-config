fc9155825e43e53a5f5658e09284b0bf5fb790d7
Nahum Sá <nahumsaa@gmail.com>
2023-11-06 20:02:35 -0300

    add bar to the right

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,25 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-vim.opt.winbar = "%=%m %f"
+local function status_line()
+  local mode = "%-5{%v:lua.string.upper(v:lua.vim.fn.mode())%}"
+  local file_name = "%-.16t"
+  local buf_nr = "[%n]"
+  local modified = " %-m"
+  local file_type = " %y"
+  local right_align = "="
+  local line_no = "%10([%l/%L%)]"
+  local pct_thru_file = "%5p%%"
+
+  return string.format(
+    "%s%s%s%s%s%s%s%s",
+    mode,
+    file_name,
+    buf_nr,
+    modified,
+    file_type,
+    right_align,
+    line_no,
+    pct_thru_file
+  )
+end
+
+-- vim.opt.statusline = status_line()
+vim.opt.winbar = status_line()
