c15f747c4be5efc2abf06a2507a241f589c00314
Nahum Sá <nahumsaa@gmail.com>
2023-11-07 22:38:05 -0300

    add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local keymap = vim.keymap.map
+
+keymap("n", "<C-d>", "<C-d>zz", { desc = "jump page down centered", remap = true })
+keymap("n", "<C-u>", "<C-u>zz", { desc = "jump page up centered", remap = true })
+keymap("v", "J", ":m '>+1<CR>gv=gv'", { desc = "move selected line up", remap = true })
+keymap("v", "J", ":m '<-2<CR>gv=gv'", { desc = "move selected line down", remap = true })
+keymap("n", "<C-f>", "<cmd>silent ! zellij-sessionizer ~<CR>", { desc = "create new zellij session", remap = true })
+keymap(
+  "n",
+  "<leader>r",
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = "replace word", remap = true }
+)
