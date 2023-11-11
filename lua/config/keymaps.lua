40cf1276e65f3642e6ca36b65d07eb09c02e7828
Nahum Sá <nahumsaa@gmail.com>
2023-11-11 09:45:11 -0300

    remove move lines keymap
    This is already done by lazy using A+{j,k}

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,6 @@ local keymap = vim.keymap.set
 
 keymap("n", "<C-d>", "<C-d>zz", { desc = "jump page down centered", remap = true })
 keymap("n", "<C-u>", "<C-u>zz", { desc = "jump page up centered", remap = true })
-keymap("v", "J", ":m '>+1<CR>gv=gv'", { desc = "move selected line up", remap = true })
-keymap("v", "J", ":m '<-2<CR>gv=gv'", { desc = "move selected line down", remap = true })
 keymap("n", "<C-f>", "<cmd>silent ! zellij-sessionizer ~<CR>", { desc = "create new zellij session", remap = true })
 keymap(
   "n",
